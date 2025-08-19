cf9c12c3d5028ee2bd7dfd6f06062caa46485961
luan <luanbispo.sousa263@gmail.com>
2025-08-19 00:30:45 -0300

    triangle of number challenge , done

--- TrianguloNumerosRepetidos.py
+++ TrianguloNumerosRepetidos.py
@@ -1,3 +1,11 @@
 numero = int(input("Número a ser repetido: "))
-for n in range(int(numero)):
-    print(numero)
+for n in range(numero , 0 , -1):
+    print(str(n) * n)
+""""
+O loop deve começar em N e ir até 1.
+
+Para cada valor i, o programa imprime o número i repetido i vezes.
+
+O uso de range(N, 0, -1) garante a contagem decrescente."""
+
+
